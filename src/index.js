d9f2ee8537b903c8ebd79b49ac2c6180bff9ade1
Benroberts37 <benrobsemail@gmail.com>
2022-02-07 15:09:08 -0700

    Pushing changes for the first 4 event types

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import './less/index.less'
 
 // Your code goes here!
+
+    // Load event type 
+  window.onload = function(event) {
+    console.log(`event ${event.type} fired! Ready to go!`)
+    const heading = document.querySelector('h1')
+    heading.textContent = 'This is event #1'
+
+    // Copy event type 
+    window.addEventListener('copy', () =>{
+        navigator.clipboard.readText()
+        .then(text =>{
+            heading.textContent += text
+        })
+    })
+    //Click event type 
+    document.body.addEventListener('click', event => {
+        event.target.classList.toggle('turnBlue')
+    })
+    //Double click event type 
+    document.body.addEventListener('dblclick', event => {
+        event.target.classList.toggle('turnRed')
+    })
+    
+}
+
