664000f3c239ee0998910fab8419e06e80441baa
Benroberts37 <benrobsemail@gmail.com>
2022-02-08 12:18:38 -0700

    Finished assignment, pushing final changes

--- src/index.js
+++ src/index.js
@@ -23,6 +23,51 @@ import './less/index.less'
     document.body.addEventListener('dblclick', event => {
         event.target.classList.toggle('turnRed')
     })
-    
+    //Keydown event type 
+    window.addEventListener('keydown', event => {
+        if (event.key === 'o') {
+            document.body.innerHTML = 'Do not press o!'
+        }
+    })
+    //Mouse move event type
+    document.body.addEventListener('mousemove', event =>{
+        const {clientX, clientY} = event
+        console.log(`Mouse is at ${clientX}, ${clientY}`)
+    })
+    //Scroll event type
+    window.addEventListener('scroll', () =>{
+        const scrollable = document.documentElement.scrollHeight - window.innerHeight;
+        const scrolled = window.scrollY;
+
+        if (Math.ceil(scrolled) === scrollable) {
+            alert('You have reached the bottom')
+        }
+    });
+    //MouseOver event type
+    const image = document.querySelector('h2');
+    image.addEventListener('mouseenter', function(event){
+        event.target.style.color = "purple";
+    })
+   
+    //Keydown event type 
+    window.addEventListener('keydown', (event) => {
+        if (event.key === 'h') {
+        alert('Hello!')
+        }
+    })
+    //blur and focus event types 
+    function pause(){
+        document.body.classList.add('paused')
+        header4.textContent = 'You lost Focus'
+    }
+
+    function play() {
+        document.body.classList.remove('paused')
+        header4.textContent = 'This has focus'
+    }
+
+    const header4 = document.querySelector('h4');
+    window.addEventListener('blur', pause)
+    window.addEventListener('focus', play);
 }
 
